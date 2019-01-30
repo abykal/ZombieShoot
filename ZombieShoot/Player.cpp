520385e650242651ff3b1597c344401a89fb7473
ABY ABRAHAM KALATHIVILA <abyabrkal@gmail.com>
2019-01-30 15:15:40 +0400

    Player class created.

--- ZombieShoot/Player.cpp
+++ ZombieShoot/Player.cpp
@@ -121,3 +121,85 @@ void Player::stopDown()
 {
 	m_DownPressed = false;
 }
+
+// Real work done here
+void Player::update(float elapsedTime, Vector2i mousePosition)
+{
+
+	if (m_UpPressed)
+	{
+		m_Position.y -= m_Speed * elapsedTime;
+	}
+
+	if (m_DownPressed)
+	{
+		m_Position.y += m_Speed * elapsedTime;
+	}
+
+	if (m_RightPressed)
+	{
+		m_Position.x += m_Speed * elapsedTime;
+	}
+
+	if (m_LeftPressed)
+	{
+		m_Position.x -= m_Speed * elapsedTime;
+	}
+
+	m_Sprite.setPosition(m_Position);
+
+
+
+	// Keep the player in the arena
+	if (m_Position.x > m_Arena.width - m_TileSize)
+	{
+		m_Position.x = m_Arena.width - m_TileSize;
+	}
+
+	if (m_Position.x < m_Arena.left + m_TileSize)
+	{
+		m_Position.x = m_Arena.left + m_TileSize;
+	}
+
+	if (m_Position.y > m_Arena.height - m_TileSize)
+	{
+		m_Position.y = m_Arena.height - m_TileSize;
+	}
+
+	if (m_Position.y < m_Arena.top + m_TileSize)
+	{
+		m_Position.y = m_Arena.top + m_TileSize;
+	}
+
+	// Calculate the angle the player is facing
+	float angle = (atan2(mousePosition.y - m_Resolution.y / 2,
+		mousePosition.x - m_Resolution.x / 2)
+		* 180) / 3.141;
+
+	m_Sprite.setRotation(angle);
+}
+
+void Player::upgradeSpeed()
+{
+	// 20% speed upgrade
+	m_Speed += (START_SPEED * .2);
+}
+
+void Player::upgradeHealth()
+{
+	// 20% max health upgrade
+	m_MaxHealth += (START_HEALTH * .2);
+
+}
+
+void Player::increaseHealthLevel(int amount)
+{
+	m_Health += amount;
+
+	// But not beyond the maximum
+	if (m_Health > m_MaxHealth)
+	{
+		m_Health = m_MaxHealth;
+	}
+}
+
