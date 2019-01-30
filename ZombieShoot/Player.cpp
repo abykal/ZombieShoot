d94c106502176e3259e287ea3d820f5abeb50878
ABY ABRAHAM KALATHIVILA <abyabrkal@gmail.com>
2019-01-30 14:47:05 +0400

    Add project files.

---
+++ ZombieShoot/Player.cpp
@@ -0,0 +1,123 @@
+#include "pch.h"
+#include "player.h"
+
+Player::Player()
+{
+	m_Speed = START_SPEED;
+	m_Health - START_HEALTH;
+	m_MaxHealth = START_HEALTH;
+
+	//Associate a texture with the sprite
+	m_Texture.loadFromFile("graphics/player.png");
+	m_Sprite.setTexture(m_Texture);
+
+	// set the origin of the sprite to the center
+	// for smooth rotation
+	m_Sprite.setOrigin(25, 25);
+}
+
+void Player::spawn(IntRect arena, Vector2f resolution, int tileSize)
+{
+	// Place the player in the middle of the arena
+	m_Position.x = arena.width / 2;
+	m_Position.y = arena.height / 2;
+
+	// Copy the details of the arena to the player's m_Arena
+	m_Arena.left = arena.left;
+	m_Arena.width = arena.width;
+	m_Arena.top = arena.top;
+	m_Arena.height = arena.height;
+
+	// Remember how big the tiles are in this arena
+	m_TileSize = tileSize;
+
+	// Strore the resolution for future use
+	m_Resolution.x = resolution.x;
+	m_Resolution.y = resolution.y;
+
+}
+
+Time Player::getLastHitTime()
+{
+	return m_LastHit;
+}
+
+bool Player::hit(Time timeHit)
+{
+	if (timeHit.asMilliseconds() - m_LastHit.asMilliseconds() > 200)// 2 tenths of second
+	{
+		m_LastHit = timeHit;
+		m_Health -= 10;
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+
+}
+
+FloatRect Player::getPosition()
+{
+	return m_Sprite.getGlobalBounds();
+}
+
+Vector2f Player::getCenter()
+{
+	return m_Position;
+}
+
+float Player::getRotation()
+{
+	return m_Sprite.getRotation();
+}
+
+Sprite Player::getSprite()
+{
+	return m_Sprite;
+}
+
+int Player::getHealth()
+{
+	return m_Health;
+}
+
+void Player::moveLeft()
+{
+	m_LeftPressed = true;
+}
+
+void Player::moveRight()
+{
+	m_RightPressed = true;
+}
+
+void Player::moveUp()
+{
+	m_UpPressed = true;
+}
+
+void Player::moveDown()
+{
+	m_DownPressed = true;
+}
+
+void Player::stopLeft()
+{
+	m_LeftPressed = false;
+}
+
+void Player::stopRight()
+{
+	m_RightPressed = false;
+}
+
+void Player::stopUp()
+{
+	m_UpPressed = false;
+}
+
+void Player::stopDown()
+{
+	m_DownPressed = false;
+}
