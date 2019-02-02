7fd50aa8e9689712e9308b010a901e9af81c738f
ABY ABRAHAM KALATHIVILA <abyabrkal@gmail.com>
2019-02-02 14:25:36 +0400

    Minor updates to code. Still Black screen.

--- ZombieShoot/ZombieShoot.cpp
+++ ZombieShoot/ZombieShoot.cpp
@@ -13,6 +13,7 @@ int main()
 {
 	// The game will always be in one of four states
 	enum class State { PAUSED, LEVELING_UP, GAME_OVER, PLAYING };
+
 	// Start with the GAME_OVER state
 	State state = State::GAME_OVER;
 
@@ -20,9 +21,9 @@ int main()
 	Vector2f resolution;
 	resolution.x = VideoMode::getDesktopMode().width;
 	resolution.y = VideoMode::getDesktopMode().height;
+	
 
-	RenderWindow window(VideoMode(resolution.x, resolution.y),
-		"Zombie Shoot", Style::Fullscreen);
+	RenderWindow window(VideoMode(resolution.x, resolution.y),"ZombieShoot", Style::Fullscreen);
 
 	// Create a an SFML View for the main action
 	View mainView(sf::FloatRect(0, 0, resolution.x, resolution.y));
